0a047ae9711010ed03ec901ff8c11dfb716f7a3b
Anvar331 <anvar.intezar@gmail.com>
2022-01-22 20:56:28 +0200

    Lesson14/level1/Task2

--- src/main/java/student_anvars_intezars/home_tasks/lesson_14/FruitStorage.java
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_14/FruitStorage.java
@@ -39,4 +39,8 @@ class FruitStorage {
         }
         return redApples;
     }
-}
+
+    //public List<Apple> findByColor(String colorToFind) {
+    //    String color = "";
+     //   for(; )
+    }
