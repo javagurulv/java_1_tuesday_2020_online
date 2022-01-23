907e39ae68d4127a14ea479e51275a751d3fc765
Anvar331 <anvar.intezar@gmail.com>
2022-01-23 16:15:54 +0200

    LessonX/Task3

---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_x/Transaction.java
@@ -0,0 +1,20 @@
+package student_anvars_intezars.home_tasks.lesson_x;
+
+public class Transaction {
+
+    private int amount;
+    private TransactionType transactionType;
+
+    public Transaction(int amount, TransactionType transactionType) {
+        this.amount = amount;
+        this.transactionType = transactionType;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
