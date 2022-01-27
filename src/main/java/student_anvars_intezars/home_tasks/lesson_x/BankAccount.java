5b10dccf4c296f09633d0793caa1ce90504bbfc1
Anvar331 <anvar.intezar@gmail.com>
2022-01-27 21:54:33 +0200

    LessonX/Task6

---
+++ src/main/java/student_anvars_intezars/home_tasks/lesson_x/BankAccount.java
@@ -0,0 +1,16 @@
+package student_anvars_intezars.home_tasks.lesson_x;
+
+public class BankAccount {
+
+    private String owner;
+    private Transaction[] transactions;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public Transaction[] getTransactions() {
+        return transactions;
+    }
+
+}
